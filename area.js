6c8eeb0c100a90fdda798756faddea5d223e0844
Aliya-Begum <aliyaansari0604@gmail.com>
2022-10-08 22:34:16 +0530

    updated area.js

--- area.js
+++ area.js
@@ -3,10 +3,15 @@ const calculateBtn = document.querySelector('#calculate');
 const output = document.querySelector('#area-Of-triangle');
 
 
+function sumOfBaseAndHeight(base, height){
+    const sum = base + height;
+    return sum;
+}
 function calculateArea(){
-   const sum = Number(inputSides[0].value) + Number(inputSides[1].value) + Number(inputSides[2].value);
-   const area = sum/2;
-   output.innerText = "Area if Triangle" +area;
+    const sum = sumOfBaseAndHeight(Number(inputSides[0].value) , Number(inputSides[1].value));
+    const area = sum/2;
+    output.innerText = "Area of the Triangle is " +area;
+
 }
 
 calculateBtn.addEventListener('click', calculateArea);
