568454f535875b48cc48fde510bcfc19a46207d3
Aliya-Begum <aliyaansari0604@gmail.com>
2022-10-17 13:22:42 +0530

    fix

--- hypotenuse.js
+++ hypotenuse.js
@@ -8,11 +8,22 @@ function calculateSumOfSquares(base, height){
     return sumOfSquares;
 }
 function calculateHypotenuse(){
-    const sumOfSquares = calculateSumOfSquares(Number(inputSides[0].value) , Number(inputSides[1].value));
-    console.log(sumOfSquares);
-    const lengthOfHypotenuse = Math.sqrt(sumOfSquares);
-    output.innerText = "The length of Hypotenuse is " +lengthOfHypotenuse;
-  
+    var inputOne = Number(inputSides[0].value);
+    var inputTwo = Number(inputSides[1].value);
+    if(inputOne && inputTwo){
+        if(inputOne > 0 && inputTwo > 0){
+
+            const sumOfSquares = calculateSumOfSquares( inputOne, inputTwo );
+            const lengthOfHypotenuse = Math.sqrt(sumOfSquares);
+        
+            output.innerText = "The length of Hypotenuse is " +lengthOfHypotenuse;
+
+        }else{
+            output.innerText = "base and height should be greater than zero(0)"
+        }
+    }else{
+        output.innerText = "Please fill out both th fields"
+    }
 }
 
 calculateBtn.addEventListener('click', calculateHypotenuse);
--- isTriangle.js
+++ isTriangle.js
@@ -3,22 +3,34 @@ const isTriangleBtn = document.querySelector('#is-triangle-btn');
 const output = document.querySelector('#output-box');
 
 function calculateSumOfTriangle(angle1, angle2, angle3) {
+
     const sumOfTriangle = angle1 + angle2 + angle3;
-    if(inputAngle[0].value&&inputAngle[1].value&&inputAngle[2].value){
-        if (sumOfTriangle === 180) {
-            output.innerText = "Yay!! Angles form a Triangle";
-        } else {
-            output.innerText = "Oh! Angles don't form a Triangle";
+
+    var angleOne = inputAngle[0].value;
+    var angleTwo = inputAngle[1].value;
+    var angleThree = inputAngle[2].value;
+
+    if(angleOne && angleTwo && angleThree){
+        if( angleOne > 0 && angleTwo > 0 && angleThree > 0){
+            if (sumOfTriangle === 180) {
+                output.innerText = "Yay!! Angles form a Triangle";
+            }
+            else{
+                output.innerText = "Oh! Angles don't form a Triangle";
+            }
         }
-        
-    }else{
-        output.innerText = "Please enter all three angles";
+        else{
+            output.innerText = "Invalid entry, Angle can not be 0. ";
+        }
     }
-    
+    else{
+        output.innerText = "Please enter all three angles."
+    }
 }
 
 function isTriangle() {
     const sumOfTriangle = calculateSumOfTriangle(Number(inputAngle[0].value), Number(inputAngle[1].value), Number(inputAngle[2].value));
+    return sumOfTriangle;
 }
 
 isTriangleBtn.addEventListener('click', isTriangle);
